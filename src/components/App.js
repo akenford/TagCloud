7e6a38b5a34c2303eb47037557c89cbfd8762f9b
Dan Mochov <danyamarow69@gmail.com>
2019-01-01 22:25:29 +0200

    ff

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1 className='caption'>My React App!</h1>
+                <h1 className='caption'>Tag Cloud</h1>
             </div>
         );
     }
