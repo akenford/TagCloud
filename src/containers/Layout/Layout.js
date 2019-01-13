fc8719cc7e4bc0117bdab31d1bff8fe0c3d46b8f
Dan Mochov <danyamarow69@gmail.com>
2019-01-14 01:32:47 +0200

    refactor-Layout-class-to-function

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,19 +1,13 @@
 // react
-import React, { PureComponent } from "react";
+import React from "react";
 import { CSSTransition } from 'react-transition-group';
 
-class Layout extends PureComponent {
-    
-    render() {
+export const Layout = ({ children }) => {
     return (
         <CSSTransition in={true} appear={true} timeout={1000} classNames="fade">
-       <div className="layout">
-            {this.props.children}
-        </div>
-    </CSSTransition>
-        
+            <div className="layout">
+                {children}
+            </div>
+        </CSSTransition>
     )
-    }
 };
-
-export default Layout;
--- src/pages/TagPage/TagPage.js
+++ src/pages/TagPage/TagPage.js
@@ -2,7 +2,7 @@
 import React, { PureComponent, Fragment } from "react";
 
 // containers
-import Layout from '../../containers/Layout/Layout'
+import { Layout } from '../../containers/Layout/Layout'
 
 // components
 import TagInfo from '../../components/TagInfo/TagInfo'
