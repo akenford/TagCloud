e35c95cf4867b8f41ac7af3a9eaa7e8cea633027
Dan Mochov <danyamarow69@gmail.com>
2019-01-14 02:10:37 +0200

    backwards-btn-prototype

--- src/pages/TagPage/TagPage.js
+++ src/pages/TagPage/TagPage.js
@@ -4,18 +4,25 @@ import React, { PureComponent, Fragment } from "react";
 // containers
 import { Layout } from '../../containers/Layout/Layout'
 
+// router
+import { Link } from "react-router-dom";
+
 // components
 import TagInfo from '../../components/TagInfo/TagInfo'
+import { Header } from '../../components/Header/Header'
 
 class TagPage extends PureComponent {
     
     render() {
     return (
-        <Fragment>
+        <div>
             <Layout>
+                {/* <Header>
+                    <Link to={"/"}>{'<='}</Link>
+                </Header> */}
                 <TagInfo/>
             </Layout>
-        </Fragment>
+        </div>
     )}
 };
 
